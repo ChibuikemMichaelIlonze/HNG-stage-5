7e78d493e9a46d8bcdc94360b7c8ef0d0edd5b3c
Chibuikem Michael Ilonze <chibuikemichaelilonze@gmail.com>
2023-09-30 23:27:04 +0100

    edit

--- src/components/popup/background.js
+++ src/components/popup/background.js
@@ -1,77 +1,34 @@
-// background.js
-let recording = false;
-let mediaRecorder;
-let recordedChunks = [];
-
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.name === "startRecording") {
-    if (!recording) {
-      chrome.desktopCapture.chooseDesktopMedia(["screen", "tab"], (streamId) => {
-        if (streamId) {
-          const streamConstraints = {
-            audio: true,
-            video: {
-              mandatory: {
-                chromeMediaSource: "desktop",
-                chromeMediaSourceId: streamId,
-                maxWidth: window.screen.width,
-                maxHeight: window.screen.height,
-              },
+const startRecording = async () => {
+    await chrome.tabs.query({'active': true, 'lastFocusedWindow': true, 'currentWindow': true}, async function (tabs) {
+      // Get current tab to focus on it after start recording on recording screen tab
+      const currentTab = tabs[0];
+  
+      // Create recording screen tab
+      const tab = await chrome.tabs.create({
+        url: chrome.runtime.getURL('recording_screen.html'),
+        pinned: true,
+        active: true,
+      });
+  
+      // Wait for recording screen tab to be loaded and send message to it with the currentTab
+      chrome.tabs.onUpdated.addListener(async function listener(tabId, info) {
+        if (tabId === tab.id && info.status === 'complete') {
+          chrome.tabs.onUpdated.removeListener(listener);
+  
+          await chrome.tabs.sendMessage(tabId, {
+            name: 'startRecordingOnBackground',
+            body: {
+              currentTab: currentTab,
             },
-          };
-
-          navigator.mediaDevices
-            .getUserMedia(streamConstraints)
-            .then((stream) => {
-              mediaRecorder = new MediaRecorder(stream);
-
-              mediaRecorder.ondataavailable = (event) => {
-                if (event.data.size > 0) {
-                  recordedChunks.push(event.data);
-                }
-              };
-
-              mediaRecorder.onstop = () => {
-                const blob = new Blob(recordedChunks, { type: "video/webm" });
-                const url = URL.createObjectURL(blob);
-
-                // Do something with the recorded video URL (e.g., save or display it)
-                console.log("Recording stopped. Video URL:", url);
-
-                // Clean up
-                recordedChunks = [];
-              };
-
-              mediaRecorder.start();
-              console.log("Recording started.");
-              recording = true;
-            })
-            .catch((error) => {
-              console.error("Error accessing media devices:", error);
-            });
+          });
         }
       });
-    } else {
-      // Stop recording
-      if (mediaRecorder && mediaRecorder.state === "recording") {
-        mediaRecorder.stop();
-        recording = false;
-        console.log("Recording stopped.");
-      }
+    });
+  };
+  
+  // Listen for startRecording message from popup.js
+  chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.name === 'startRecording') {
+      startRecording();
     }
-  }
-
-  // Handle camera toggle
-  if (message.name === "togglecamera") {
-    // Handle camera toggle logic here
-    const isCameraEnabled = message.isEnabled;
-    // Update camera state as needed
-  }
-
-  // Handle audio toggle
-  if (message.name === "toggleaudio") {
-    // Handle audio toggle logic here
-    const isAudioEnabled = message.isEnabled;
-    // Update audio state as needed
-  }
-});
+  });
